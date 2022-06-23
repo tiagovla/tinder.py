602617415d4e7d29ed2ea3966952655faec2ac69
tiagovla <tiagovla@gmail.com>
2022-06-23 02:56:43 -0300

    ci: fix black formatting

--- tinder/models/asset.py
+++ tinder/models/asset.py
@@ -3,7 +3,7 @@ import os
 from typing import Union
 
 from collections import OrderedDict
-from tinder.errors import  TinderException
+from tinder.errors import TinderException
 from tinder.state import ConnectionState
 
 
