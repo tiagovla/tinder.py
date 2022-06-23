cd07241c4896e87d8ee84f097095d1363299c0ea
tiagovla <tiagovla@gmail.com>
2022-06-22 23:41:29 -0300

    test: add http.Route tests

---
+++ tests/test_http.py
@@ -0,0 +1,22 @@
+from tinder import http
+from tinder.errors import HTTPException
+import pytest
+import asyncio
+
+
+def test_route_get_method():
+    r_get = http.Route("GET", "/test")
+    assert r_get.method == "GET"
+    assert r_get.url == "https://api.gotinder.com/test"
+
+
+def test_route_post_method():
+    r_post = http.Route("POST", "/test")
+    assert r_post.method == "POST"
+    assert r_post.url == "https://api.gotinder.com/test"
+
+
+def test_route_format():
+    r = http.Route("GET", "/test/{test}", test="working")
+    assert r.method == "GET"
+    assert r.url == "https://api.gotinder.com/test/working"
